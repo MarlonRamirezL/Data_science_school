414a2e6894985e76897571e765393c356b066660
Marlon <marlon.ramirezlizcano@gmail.com>
2020-08-07 17:25:43 -0500

    My first program using functions

---
+++ Curso_Básico_de_Python/functions.py
@@ -0,0 +1,14 @@
+def conversation(message):
+    print('Hello')
+    print('How are you?')
+    print(message)
+    print('Good bye')
+
+option= int(input('Choose an option(1, 2, 3): '))
+if option == 1:
+    conversation('You chose option 1')
+elif option == 2:
+    conversation('You chose option 2')
+elif option == 3:
+    conversation('You chose option 3')
+else: print('Choose a correct answer (1, 2 or 3)')
