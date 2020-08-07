b6462db6a2258e8c33bba943b23324cef95ec34b
Marlon <marlon.ramirezlizcano@gmail.com>
2020-08-07 18:14:12 -0500

    Improvement #2, using functions

--- Curso_Básico_de_Python/currency_converter.py
+++ Curso_Básico_de_Python/currency_converter.py
@@ -1,3 +1,9 @@
+def converter(pesos_type,dolar_value):
+    pesos= float(input("How many " + pesos_type + " pesos do you have? :"))
+    dolars= pesos / dolar_value
+    dolars= round(dolars, 2)
+    dolars= str(dolars)
+    print("You have $ " + dolars + " dolars")
 menu = """
 Welcome to your currency converter
 
@@ -10,26 +16,13 @@ Choose one option: """
 option = int(input(menu))
 
 if option == 1:
-    pesos= float(input("How many colombian pesos do you have?:"))    
-    dolar_value= 3782
-    dolars= pesos / dolar_value
-    dolars= round(dolars, 2)
-    dolars= str(dolars)
-    print("You have $ " + dolars + " dolars")
+    converter('Colombian',3872) 
+
 elif option == 2:
-    pesos= float(input("How many argentine pesos do you have?:"))    
-    dolar_value= 65
-    dolars= pesos / dolar_value
-    dolars= round(dolars, 2)
-    dolars= str(dolars)
-    print("You have $ " + dolars + " dolars")
+    converter('Argentine',73)
+
 elif option == 3:
-    pesos= float(input("How many mexican pesos do you have?:"))    
-    dolar_value=24
-    dolars= pesos / dolar_value
-    dolars= round(dolars, 2)
-    dolars= str(dolars)
-    print("You have $ " + dolars + " dolars")
+    converter('Mexican',23)
 
 else:
     print("Choose a correct answer please")
