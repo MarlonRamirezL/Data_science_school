40331e6b0456b05ac0c98d6800e834b0d75a3a59
Marlon <marlon.ramirezlizcano@gmail.com>
2020-08-07 17:46:13 -0500

    Functions with return

--- Curso_Básico_de_Python/functions.py
+++ Curso_Básico_de_Python/functions.py
@@ -1,14 +1,23 @@
-def conversation(message):
-    print('Hello')
-    print('How are you?')
-    print(message)
-    print('Good bye')
+#def conversation(message):
+    #print('Hello')
+    #print('How are you?')
+    #print(message)
+    #print('Good bye')
 
-option= int(input('Choose an option(1, 2, 3): '))
-if option == 1:
-    conversation('You chose option 1')
-elif option == 2:
-    conversation('You chose option 2')
-elif option == 3:
-    conversation('You chose option 3')
-else: print('Choose a correct answer (1, 2 or 3)')
+#option= int(input('Choose an option(1, 2, 3): '))
+#if option == 1:
+    #conversation('You chose option 1')
+#elif option == 2:
+    #conversation('You chose option 2')
+#elif option == 3:
+    #conversation('You chose option 3')
+#else: print('Choose a correct answer (1, 2 or 3)')
+
+
+def suma(a,b):
+    print('Se suman dos números')
+    resultado = a + b
+    return resultado
+
+sumatoria=suma(3,8)
+print(sumatoria)
