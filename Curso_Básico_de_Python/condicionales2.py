5d83ba5fd7edf329487429d7b1ef6e2a4a2282e5
Marlon <marlon.ramirezlizcano@gmail.com>
2020-08-07 15:03:56 -0500

    Using elif for the firs time

--- Curso_Básico_de_Python/condicionales2.py
+++ Curso_Básico_de_Python/condicionales2.py
@@ -1,7 +1,16 @@
-age = int(input("How old are you?"))
-if age > 17 :
-    print("You are an adult")
-else :
-    print("You are not an adult")
+# age = int(input("How old are you?"))
+# if age > 17 :
+#    print("You are an adult")
+# else :
+#    print("You are not an adult")
+
+number = int(input('Write any number: '))
+
+if number > 5:
+    print('It is greater than 5')
+elif number == 5:
+    print('It is equal to 5')
+else:
+    print ('It is smaller than 5')
 
 
