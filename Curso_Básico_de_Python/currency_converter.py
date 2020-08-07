63c65542a6c05f66f9948b0a9c871329da248b04
Marlon <marlon.ramirezlizcano@gmail.com>
2020-08-07 15:15:20 -0500

    Using elif for the firs timels

--- Curso_Básico_de_Python/currency_converter.py
+++ Curso_Básico_de_Python/currency_converter.py
@@ -1,7 +1,20 @@
-pesos= input("How many colombian pesos do you have?:")
-pesos= float(pesos)
-dolar_value= 3782
-dolars= pesos / dolar_value
-dolars= round(dolars, 2)
-dolars= str(dolars)
-print("You have $ " + dolars + " dolars")
+menu = """
+Welcome to your currency converter
+
+1- Colombian pesos.
+2- Argentine pesos.
+3- Mexican pesos.
+
+Choose one option: """
+
+option = input(menu)
+
+
+
+#pesos= input("How many colombian pesos do you have?:")
+#pesos= float(pesos)
+#dolar_value= 3782
+#dolars= pesos / dolar_value
+#dolars= round(dolars, 2)
+#dolars= str(dolars)
+#print("You have $ " + dolars + " dolars")
