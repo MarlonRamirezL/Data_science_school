5951b19a47ef70234f660121a677cfadd4502c17
Marlon <marlon.ramirezlizcano@gmail.com>
2020-08-07 16:30:57 -0500

    Improvements #1, using if, elif and else

--- Curso_Básico_de_Python/currency_converter.py
+++ Curso_Básico_de_Python/currency_converter.py
@@ -7,14 +7,31 @@ Welcome to your currency converter
 
 Choose one option: """
 
-option = input(menu)
+option = int(input(menu))
+
+if option == 1:
+    pesos= float(input("How many colombian pesos do you have?:"))    
+    dolar_value= 3782
+    dolars= pesos / dolar_value
+    dolars= round(dolars, 2)
+    dolars= str(dolars)
+    print("You have $ " + dolars + " dolars")
+elif option == 2:
+    pesos= float(input("How many argentine pesos do you have?:"))    
+    dolar_value= 65
+    dolars= pesos / dolar_value
+    dolars= round(dolars, 2)
+    dolars= str(dolars)
+    print("You have $ " + dolars + " dolars")
+elif option == 3:
+    pesos= float(input("How many mexican pesos do you have?:"))    
+    dolar_value=24
+    dolars= pesos / dolar_value
+    dolars= round(dolars, 2)
+    dolars= str(dolars)
+    print("You have $ " + dolars + " dolars")
+
+else:
+    print("Choose a correct answer please")
 
 
-
-#pesos= input("How many colombian pesos do you have?:")
-#pesos= float(pesos)
-#dolar_value= 3782
-#dolars= pesos / dolar_value
-#dolars= round(dolars, 2)
-#dolars= str(dolars)
-#print("You have $ " + dolars + " dolars")
