e3022c3e3ee416d46a720ab4f30e8b68dd81fc7d
Marlon <marlon.ramirezlizcano@gmail.com>
2020-08-08 13:38:31 -0500

    First time using the for loop

---
+++ Curso_Básico_de_Python/multiplication_tables.py
@@ -0,0 +1,15 @@
+def multiplication_table(number):
+    
+    for i in range(1,13):
+        result = number * i
+        print(f'{number} x {i} ={result}')
+
+def run():
+    menu="""
+    Welcome 
+    """
+    number= int(input('Write a number to see its multiplication table: '))
+    multiplication_table(number)  
+
+if __name__ == "__main__":
+    run()
